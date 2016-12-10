b5067637177ff7fdd900e0c2e606ee81cdabe577
Andrew <andrew@laytipster.co.uk>
2016-12-10 19:07:03 +0000

    Initial commit

---
+++ demoMVC/Domain/FIleStoreInsert.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using LiteDB;
+using System.IO;
+
+namespace demoMVC.Domain
+{
+    public class FIleStoreInsert
+    {
+        public static string Gallery {
+            get { return "$/Gallery/"; }
+        }
+
+        public static void insertImages()
+        {
+            using (var db = new LiteDatabase(DbConnection.FileStorageLocation))
+            {
+                var path = @"C:\Users\Learning Clip\Documents\DotNetMVCInsertImagedb\demoMVC\Images\";
+                var filePaths = Directory.GetFiles(path, "*.jpg");
+                List<string> list = new List<string>();
+                list.AddRange(filePaths);
+                foreach (var file in list)
+                {
+                    var ext = Path.GetExtension(file);
+                    var filename = Guid.NewGuid();
+                    db.FileStorage.Upload(Gallery + filename + ext, file);
+                }
+            }
+        }
+    }
+}
